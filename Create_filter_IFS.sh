03621713eca13edc0c4682f17bebf8716240123b
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-05-22 13:31:58 +0100

    Update Create_filter_IFS.sh

--- Create_filter_IFS.sh
+++ Create_filter_IFS.sh
@@ -5,10 +5,14 @@
 # Create the 'log' directory
 mkdir -p log
 
+## Crear jobs
 sbatch --qos=high --output=MKIFS.out --error=MKIFS.err --job-name=MKIFS -n 8 --time=23:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt"
 
 # Run the LiCSAR_03_mk_ifgs.py script with the given arguments
-LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt
+#LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt
 
 # Notify the user of completion
 echo "Log directory created and LiCSAR_03_mk_ifgs.py executed successfully."
+
+rm -rf 13_doGeocoding
+
