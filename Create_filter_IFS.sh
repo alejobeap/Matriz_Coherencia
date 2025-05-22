5ec327e1a1f55eb8fd4ddf1916178fcb7dd48440
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-05-22 13:30:40 +0100

    Update Create_filter_IFS.sh

--- Create_filter_IFS.sh
+++ Create_filter_IFS.sh
@@ -5,6 +5,8 @@
 # Create the 'log' directory
 mkdir -p log
 
+sbatch --qos=high --output=MKIFS.out --error=MKIFS.err --job-name=MKIFS -n 8 --time=23:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt"
+
 # Run the LiCSAR_03_mk_ifgs.py script with the given arguments
 LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt
 
