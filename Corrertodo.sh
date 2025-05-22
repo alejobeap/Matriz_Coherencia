0f0642f95d2088e932d0ee1afbc42d9b945e7d7b
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-05-22 19:03:28 +0100

    Add files via upload

---
+++ Corrertodo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Uso: $0 <nombre_carpeta>"
+  exit 1
+fi
+
+NOMBRE=$1
+
+NUMERO=$(python3 VER_Nombre_volcan.py "$NOMBRE" | tr -d '[]')
+if [ $? -ne 0 ] || [ -z "$NUMERO" ]; then
+  echo "Error ejecutando VER_Nombre_volcan.py o valor vacío"
+  exit 1
+fi
+
+echo "Número obtenido: $NUMERO"
+
+mkdir -p "$NOMBRE"
+cd "$NOMBRE" || { echo "No se pudo entrar a la carpeta $NOMBRE"; exit 1; }
+
+RUTA_BASE="/gws/nopw/j04/nceo_geohazards_vol1/projects/LiCS/proc/current/subsets/volc/$NUMERO"
+
+CARPETAS=( $(ls -d "$RUTA_BASE"/*/ 2>/dev/null | xargs -n 1 basename) )
+
+if [ ${#CARPETAS[@]} -eq 0 ]; then
+  echo "No se encontraron carpetas dentro de $RUTA_BASE"
+  exit 1
+fi
+
+TOTAL_PASOS=$((${#CARPETAS[@]} * 3))
+PASO_ACTUAL=0
+
+for CARPETA in "${CARPETAS[@]}"; do
+
+
+  # Al final de cada carpeta, clonar repo y mover contenido
+  echo "Creando IFS"
+  (
+    cd "$CARPETA" || { echo "No se pudo entrar a $CARPETA"; exit 1; }
+    chmod +x *.sh
+    ./RSLC2listIFS.sh
+    #./Create_filter_IFS.sh
+    sbatch --qos=high --output=MKIFS_$CARPETA.out --error=MKIFS_$CARPETA.err --job-name=MKIFS_$CARPETA -n 8 --time=23:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt"
+  )
+done
+
+echo "Proceso finalizado."
+
