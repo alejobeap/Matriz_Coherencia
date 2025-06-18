cee804bb800c76ad0bddb3e6f1713807fd4c4a6e
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-06-18 19:18:39 +0100

    Update Create_filter_IFS.sh

--- Create_filter_IFS.sh
+++ Create_filter_IFS.sh
@@ -10,7 +10,7 @@ mkdir -p GEOC
 
 
 ## Crear jobs
-sbatch --qos=high --output=MKIFS_${name}_${tracks}.out --error=MKIFS_${name}_${tracks}.err --job-name=MKIFS_${name}_${tracks} -n 8 --time=23:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i combination.txt"
+sbatch --qos=high --output=MKIFS_${name}_${tracks}.out --error=MKIFS_${name}_${tracks}.err --job-name=MKIFS_${name}_${tracks} -n 8 --time=23:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i combination_longs.txt"
 
 # Run the LiCSAR_03_mk_ifgs.py script with the given arguments
 #LiCSAR_03_mk_ifgs.py -d . -r 7 -a 2 -c 0 -i filtered_date_pairs.txt
